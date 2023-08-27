5b34195723f6f9d8305db51b5bf355d0319603dc
quantumopticss <143305958+quantumopticss@users.noreply.github.com>
2023-08-27 12:32:10 +0800

    Update group_ode.m

--- group_ode.m
+++ group_ode.m
@@ -1,11 +1,11 @@
 function fun = group_ode(t,x)
 
-%clist = [1,1,1,1,1,2.40482555769577];
+%clist = [1,1,1,1,2.40482555769577];
 E = 1;%clist(1,1);
-q = 1;%cLlist(1,2);
-m = 1;%c
-w = 1;%
-B = 1;
+q = 1;%clist(1,2);
+m = 1;%clist(1,3)
+w = 1;%clist(1,4)
+B = 1;%clist(1,5)
 % lambda = q*B/(m*w);  %core2
 Ae = q*E*cos(w*t)/m;
 Be = q*B*sin(w*t)/m;
